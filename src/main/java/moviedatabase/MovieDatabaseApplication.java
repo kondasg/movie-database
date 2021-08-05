9edb68d38434dd53d6e3db63f5be6d3bf24da7db
kondasg <gabor@kondas.hu>
2021-08-05 23:33:13 +0200

    add dependencies and config

--- src/main/java/moviedatabase/MovieDatabaseApplication.java
+++ src/main/java/moviedatabase/MovieDatabaseApplication.java
@@ -1,7 +1,11 @@
 package moviedatabase;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MovieDatabaseApplication {
@@ -10,4 +14,17 @@ public class MovieDatabaseApplication {
         SpringApplication.run(MovieDatabaseApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Movie Database API")
+                        .version("1.0.0")
+                        .description("Movie Database API"));
+    }
 }
